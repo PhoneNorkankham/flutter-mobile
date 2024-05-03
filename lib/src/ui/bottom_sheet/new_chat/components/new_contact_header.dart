7ed746572b0c9d15cb182ca13a1599730e840fe2
Quyết Thắng <thang13513110061@gmail.com>
2024-05-03 20:58:29 +0700

    chore: Updated color

--- lib/src/ui/bottom_sheet/new_chat/components/new_contact_header.dart
+++ lib/src/ui/bottom_sheet/new_chat/components/new_contact_header.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:keepup/src/design/components/avatars/app_circle_avatar.dart';
-import 'package:keepup/src/design/themes/extensions/theme_extensions.dart';
 
 class NewContactHeader extends StatelessWidget {
   const NewContactHeader({super.key});
@@ -9,34 +8,8 @@ class NewContactHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Theme.of(context).scaffoldBackgroundColor,
-      constraints: const BoxConstraints(minHeight: 216),
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(height: 26),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Icon(
-                Icons.av_timer,
-                color: Theme.of(context).colorScheme.onPrimary,
-              ),
-              Expanded(
-                child: Text(
-                  '0 Days left',
-                  style: context.appTextTheme.medium14.copyWith(
-                    color: Theme.of(context).colorScheme.onPrimary,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 20),
-          const AppCircleAvatar(url: '', radius: 50),
-          const SizedBox(height: 26),
-        ],
-      ),
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 30),
+      child: const Center(child: AppCircleAvatar(url: '', radius: 50)),
     );
   }
 }
