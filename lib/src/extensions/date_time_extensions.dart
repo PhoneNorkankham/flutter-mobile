15cd3b45552b4dd9f795fb8d4a19fb74cf694ada
Quyết Thắng <thang13513110061@gmail.com>
2024-10-04 22:57:19 +0700

    keepup - today and upcoming view tweaks
    Description
    
    Today list should show all contacts that have 1 day until expiration AND expired (we might add a filter to see today versus expired).
    
    Upcoming should contain one list total. It should NOT contain any contacts that are expiring today. It should be more of a simple list of all. The query and sorting logic are as follows. Display all contacts in the order of expiration ascending (1,2,3days etc). Excluding expired. Since expired will be on the today view.
    
    https://www.loom.com/share/eb93dc71fd764ec5b764abadbc1b377b

--- lib/src/extensions/date_time_extensions.dart
+++ lib/src/extensions/date_time_extensions.dart
@@ -13,4 +13,42 @@ extension DateTimeExtensions on DateTime {
   DateTime get tomorrow => DateUtils.addDaysToDate(this, 1);
 
   bool get isTomorrow => DateUtils.isSameDay(toLocal(), DateTime.now().tomorrow);
+
+  // Function to check if a date is before "the day after tomorrow"
+  bool get isDateBeforeDayAfterTomorrow {
+    DateTime dateOnly = DateUtils.dateOnly(this); // Remove time in this date
+    DateTime now = DateUtils.dateOnly(DateTime.now()); // Remove time in today
+    // The day after tomorrow is two days after today.
+    DateTime dayAfterTomorrow = now.add(const Duration(days: 2));
+
+    return dateOnly.isBefore(dayAfterTomorrow);
+  }
+
+  // Function to check if a date is in the current week (starting from the day after tomorrow)
+  bool get isDateInCurrentWeekFromDayAfterTomorrow {
+    DateTime dateOnly = DateUtils.dateOnly(this); // Remove time in this date
+    DateTime now = DateUtils.dateOnly(DateTime.now()); // Remove time in today
+    DateTime dayAfterTomorrow = now.add(const Duration(days: 2));
+
+    // Calculate the first and last day of the week in the current week
+    DateTime startOfWeek = now.subtract(Duration(days: now.weekday - 1)); // Monday
+    DateTime endOfWeek = startOfWeek.add(const Duration(days: 6)); // Sunday
+
+    return dateOnly.isAfter(dayAfterTomorrow.subtract(const Duration(days: 1))) &&
+        dateOnly.isBefore(endOfWeek.add(const Duration(days: 1)));
+  }
+
+  // Function to check if a date is in the current month and starts from the next week
+  bool get isDateInNextWeekAndThisMonth {
+    DateTime dateOnly = DateUtils.dateOnly(this); // Remove time in this date
+    DateTime now = DateUtils.dateOnly(DateTime.now()); // Remove time in today
+
+    // Calculate the first day of the next week (Monday next week)
+    DateTime startOfNextWeek = now.add(Duration(days: 7 - now.weekday + 1));
+
+    // Check if a date is in the current month and after or equal to the first day of the next week
+    return month == now.month &&
+        year == now.year &&
+        dateOnly.isAfter(startOfNextWeek.subtract(const Duration(days: 1)));
+  }
 }
